da9f58b8d5f2a14a41a35081a1002c15870799e4
1mike12 <1mike12@gmail.com>
2020-08-16 10:25:11 -0400

    decryption test works

--- test/index.js
+++ test/index.js
@@ -1,15 +1,29 @@
 const fs = require("fs").promises;
-const cryptography = require("../lib/cryptography")
+const cryptography = require("../lib/cryptography");
 const PASS = "password";
+const path = require("path");
+const expect = require("chai").expect;
 
 describe("cryptography", ()=> {
-    it("should decrypt files", async()=> {
-        cryptography.encrypt({
+    it("should encrypt and decrypt files", async()=> {
+
+        const rawFile = path.join(__dirname , "./test.env")
+        const encryptedFile = path.join(__dirname , "./test.env.enc")
+        await cryptography.encrypt({
             secret: PASS,
-            inputFile: "",
-            outputFilePath: "",
+            inputFile: rawFile,
+            encryptedFile: encryptedFile,
             encryptionAlgo: "aes256",
             ivLength: 16
         })
+
+        const decrypted = cryptography.decrypt({
+            secret: PASS,
+            encryptedFile: encryptedFile,
+            encryptionAlgo: "aes256",
+            ivLength: 16
+        })
+
+        expect(decrypted).equal("foo=bar")
     })
 })
